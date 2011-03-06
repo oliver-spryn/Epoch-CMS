47a4f247197434ccbb37dc652c4078817656eab8
Oliver Spryn <4b394b0f@opayq.com>
2011-03-06 04:45:26 +0000

    Initial version

---
+++ system/library/database.class.php
@@ -0,0 +1,183 @@
+<?php
+/*
+LICENSE: http://docs.forwardfour.com/index.php/License
+
+Package: System Core
+Dependencies: config.php, index.php, messages.class.php
+Known issues: None
+
+Create a connection to the local database, and provide all of the necessary methods to safely create, read, update, and delete data from the database.
+*/
+	
+	class Database {
+		private $message;
+		private $connection;
+		
+		function __construct() {
+			global $config;
+			
+		/*
+		 * We will need to access to the contents of the "Message" class, in the case that a connection or query would fail, so create a private "$message" class.
+		 * This constructor method is not using the global "Message" class because this is the only time which stylesheets will not be present in the case of an error.
+		 * All other database-related errors will have a stylesheet present, so they will refer to the global "Message" class.
+		*/
+			$this->message = new Message(true);
+			
+		//Try connecting to the database server
+			$this->connection = mysql_connect($config->dbHost . ":" . $config->dbPort, $config->dbUserName, $config->dbPassword);
+			
+		//Check to see if the connection was successful
+			if (!$this->connection) {
+				$this->message->error("<strong>Fatal error:</strong> The system could not connect to the database server. Please ensure that your credentials are correct, and that the server is not offline.\n<br /><br />\n" . mysql_error());
+				exit;
+			}
+			
+		//Try selecting the database
+			$dbSelect = mysql_select_db($config->dbName, $this->connection);
+			
+		//Check to see if the selection was successful
+			if (!$dbSelect) {
+				$this->message->error("<strong>Fatal error:</strong> The system could not select the database. Please ensure that your database name is correct.\n<br /><br />\n" . mysql_error());
+				exit;
+			}
+		}
+		
+	//Clean-up escaped database values which will were recently pulled database prior to displaying.
+		public function prepare($input, $htmlEncode = false, $stripSlashes = true) {
+			global $message;
+			
+			if ($stripSlashes == true) {
+			//Strip the slashes and return the html entities of a string
+				if ($htmlEncode == true) {
+					return htmlentities(stripslashes($input));
+			//Only strip the slashes of the string, DEFAULT BEHAVIOR
+				} else {
+					return stripslashes($input);
+				}
+			} else {
+			//Only return the html entities of a string
+				if ($htmlEncode == true) {
+					return htmlentities($input);
+				} else {
+					$error = debug_backtrace();
+					$message->error("<strong>Warning:</strong> No action was performed on the returned database value on line " .  $error['0']['line']);
+				}
+			}
+		}
+		
+	/*
+	 * This method completes the following tasks:
+	 *  - run a "mysql_fetch_array()" command when the query selects all columns, or "mysql_fetch_assoc" when only a few columns are selected
+	 *  - prepare images and URLs for SSL support
+	 *  - clean-up escaped database values
+	*/
+		public function fetch($value, $fetchType = "array") {
+			if ($fetchType == "array") {
+				$result = mysql_fetch_array($value);
+			} else {
+				$result = mysql_fetch_assoc($value);
+			}
+			
+			if ($result) {
+				$return = array();
+				
+				foreach ($result as $key => $value) {
+					if (PROTOCOL == "https://") {	
+						$return[$key] = str_replace(str_replace("https://", "http://", ROOT), ROOT, $this->prepare($value));
+					} else {
+						$return[$key] = $this->prepare($value);
+					}
+				}
+				
+				return $return;
+			} else {
+				return false;
+			}
+		}
+		
+	/*
+	 * This method completes the following tasks:
+	 *  - run a "mysql_query()" command
+	 *  - ensure the query was run successfully
+	 *  - fetch an array (using the fetch() method), return a raw resource, count the number of returned values, or return an array when the given query selects only a only a few specific columns (again, using the fetch() method)
+	*/
+		public function query($query, $returnType = false, $showError = true) {
+			$action = mysql_query($query, $this->connection);
+			
+		//If no value was returned from the query
+			if (!$action) {
+			//If allowed to display an error
+				if ($showError == true) {
+					$error = debug_backtrace();
+					$this->message->error("<strong>Warning:</strong> There is an error with your query: \n" . $query . "\n<br /><br />\n" . mysql_error() . "\n<br /><br />\nError on line: " . $error['0']['line'] . "<br />\nError in file: " . $error['0']['file']);
+				} else {
+					return false;
+				}
+			} else {
+			//If the following words are used inside of the query, then a command was executed on the database, and no value needs to be returned.
+				if (!strstr($query, "INSERT INTO") && !strstr($query, "UPDATE") && !strstr($query, "SET") && !strstr($query, "CREATE TABLE") && !strstr($query, "ALTER TABLE") && !strstr($query, "DROP TABLE")) {
+					switch($returnType) {
+					//Fetch an array, and clean-up each value for display, DEFAULT BEHAVIOR
+						case false : 
+						case "array" : 
+							if ($result = fetch($action)) {
+								//Do nothing, the array was sucessfully extracted
+							} else {
+								if ($showError == true) {
+									$error = debug_backtrace();
+									$this->message->error("<strong>Warning:</strong> There is an error with your query: \n" . $query . "\n<br /><br />\nError on line: " . $error['0']['line'] . "<br />\nError in file: " . $error['0']['file']);
+								}
+							}
+							
+							if (is_array($result) && !empty($result)) {
+								return $result;
+							} else {
+								return false;
+							}
+							
+							break;
+							
+					//Return the raw resource
+						case "raw" : 
+							return $action;
+							break;
+								
+					//Return the number of rows
+						case "num" : 
+							$result = mysql_num_rows($action);
+							return $result;
+							break;
+							
+					//Fetch the array, where only certain rows are selected
+						case "selected" : 
+							if ($result = fetch($action, "assoc")) {
+								//Do nothing, the array was sucessfully extracted
+							} else {
+								if ($showError == true) {
+									$error = debug_backtrace();
+									$this->message->error("<strong>Warning:</strong> There is an error with your query: \n" . $query . "\n<br /><br />\nError on line: " . $error['0']['line'] . "<br />\nError in file: " . $error['0']['file']);
+								}
+							}
+							
+							if (is_array($result) && !empty($result)) {
+								return $result;
+							} else {
+								return false;
+							}
+							
+							break;
+							
+					//Return an error if an unsupported return-type is requested
+						default : 
+							$error = debug_backtrace();
+							$this->message->error("<strong>Warning:</strong> An invalid query return-type was requested on line " .  $error['0']['line']);
+							break;
+					}
+				}
+			}
+		}
+	}
+	
+//Instantiate the "Database" class to allow the system easily communicate with the database.
+	$database = new Database();
+	$db = $database;
---
+++ system/library/messages.class.php
@@ -0,0 +1,94 @@
+<?php
+/*
+LICENSE: http://docs.forwardfour.com/index.php/License
+
+Package: System Core
+Dependencies: None
+Known issues: None
+
+This class will build success and alert messages.
+*/
+
+	class Message {
+	/*
+	 * Check to see if the message boxes should be styled using inline styles, or external stylesheets by using the constructor method.
+	 * The only time inline styles should be used is when the system is displaying a fatal error which occured before the page content could be generated.
+	*/
+		function __construct($input) {
+			$useStyleSheet = $input;
+		}
+		
+	//Display a success message
+		public function success($message) {
+			if ($this->useStyleSheet == false) {
+				$return = "<style type=\"text/css\">
+  .ui-widget { font-family: Verdana,Arial,sans-serif; font-size: 1.1em; }
+  .ui-state-highlight { border: 1px solid #fcefa1; background: #fbf9ee url(" . ROOT . "system/images/ajax_libraries/base/ui-bg_glass_55_fbf9ee_1x400.png) 50% 50% repeat-x; color: #363636; }
+  .ui-corner-all { -moz-border-radius: 4px; -webkit-border-radius: 4px; }
+  .ui-icon { width: 16px; height: 16px; background-image: url(" . ROOT . "system/images/ajax_libraries/base/ui-icons_222222_256x240.png); }
+  .ui-icon-info { background-position: -16px -144px; }
+</style>";
+			} else {
+				$return = "";
+			}
+			
+			$return .= "
+<div class=\"ui-widget\">
+<div class=\"ui-state-highlight ui-corner-all\" style=\"margin-top: 20px; padding: 0pt 0.7em;\"> 
+<p><span class=\"ui-icon ui-icon-info\" style=\"float: left; margin-right: 0.3em;\"></span>
+" . $message . "</p>
+</div>
+</div>";
+			
+			echo $return;
+		}
+		
+	//Display an error message
+		public function error($message) {
+			if ($this->useStyleSheet == false) {
+				$return = "<style type=\"text/css\">
+  .ui-widget { font-family: Verdana,Arial,sans-serif; font-size: 1.1em; }
+  .ui-state-error { border: 1px solid #cd0a0a; background: #fef1ec url(" . ROOT . "system/images/ajax_libraries/base/ui-bg_glass_95_fef1ec_1x400.png) 50% bottom repeat-x; color: #cd0a0a; }
+  .ui-corner-all { -moz-border-radius: 4px; -webkit-border-radius: 4px; }
+  .ui-icon { width: 16px; height: 16px; background-image: url(" . ROOT . "system/images/ajax_libraries/base/ui-icons_222222_256x240.png); }
+  .ui-icon-alert { background-position: 0 -144px; }
+</style>";
+			} else {
+				$return = "";
+			}
+			
+			$return .= "
+<div class=\"ui-widget\">
+<div class=\"ui-state-error ui-corner-all\" style=\"margin-top: 20px; padding: 0pt 0.7em;\"> 
+<p><span class=\"ui-icon ui-icon-alert\" style=\"float: left; margin-right: 0.3em;\"></span>
+" . $message . "</p>
+</div>
+</div>";
+			
+			echo $return;
+		}
+		
+	//Generate a message based on URL parameters
+		public function generate($trigger, $triggerValue, $type, $text) {
+			if (isset($_GET[$trigger]) && $_GET[$trigger] == $triggerValue) {
+				switch($type) {
+					case "success" :
+						$this->success($text);
+						break;
+						
+					case "error" : 
+						$this->error($text);
+						break;
+						
+					default : 
+						$error = debug_backtrace();
+						$this->error("<strong>Warning:</strong> An invalid message type was requested on line " .  $error['0']['line']);
+						break;
+				}
+			}
+		}
+	}
+	
+//Instantiate the "Message" class to allow the system to easily display messages to the user.
+	$message = new Message(true);
+?>
