603750c8e35c3a6a18c947194d56fb1893be62ba
Oliver Spryn <4b394b0f@opayq.com>
2011-03-22 20:26:59 +0000

    Introduced debug mode

--- data/system/config.php
+++ data/system/config.php
@@ -30,5 +30,6 @@ This script is created during the automated setup process, and contains the core
 		public $folderPermissions = "0777";
 		public $encryptedSalt = "%(*&NSJ(&jd&81245";
 		public $sessionSuffix = "HJF789HF6";
+		public $debugMode = false;
 	}
 ?>
