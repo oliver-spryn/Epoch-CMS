14c9263c4bbe8725df1ee19dc920119da268000d
Oliver Spryn <4b394b0f@opayq.com>
2011-10-13 23:08:05 +0000

    Code in this revision was cleaned up, rewritten, and documented in a more concise manner. Also, several constants, improved error checking, and an import() function were added for better performance and easier future development.

--- system/server/index.php
+++ system/server/index.php
@@ -18,17 +18,17 @@
  * This script is the super core of the system, which prepares the values from the
  * configuration script for use within the system. Here is an overview of this
  * relatively simple script:
- *  [1] The server checks to see if at least PHP 5 is running, then defines several
+ *  [1] The server is examined to see which operating system it is running, and will
+ *      then define a constant giving the name of the operating system, and whether or
+ *      not the operating system is Windows or *nix, to the rest of the application.
+ *  [2] The server checks to see if at least PHP 5 is running, and also defines several
  *      constants which will make the major and minor versions avaliable to the
  *      system.
- *  [2] Check to see if the configuration script exists, and display a message if
- *  	it does not.
- *  [3] Instantiate the "Config" class for use through out this script and system.
- *  [4] Windows directory paths use a backslash. However, all other operating systems
- *      use a forward slash. This step uses the configuration file to set whether or
- *      slashes should be forward or back. 
- *  [5] Define several constants which will define local and CDN-based URLs for system-
- *  	wide use.
+ *  [3] Check to see if the configuration script exists, and redirect to the installer
+ *      if it does not.
+ *  [4] Instantiate the "Config" class for use through out this script and system.
+ *  [5] Check to see if the browser made a request via the HTTPS protocol, and set a
+ *      constant to track this and other URL-related data.
  *  [6] Include the essential classes within the system's core.
  *  [7] Start a session.
  *  [8] Set several several configurations which will boost performance, improve
@@ -36,30 +36,47 @@
  *  [9] Create a function which be used to import additional classes and packages into
  *  	a script for parsing, using ECMAScript standards.
  */
-
+ 
+//Check for the operating system that the server is running
+	define("SERVER_OS", php_uname("s"));
+	define("IS_WINDOWS", SERVER_OS == "Windows NT" ? true : false);
+	
 //This system requires a minimum of PHP 5, so ensure that this condition is true before doing else anything!
-	$PHPVersionInfo = phpversion();
-	!defined("PHP_MAJOR_VERSION") ? define("PHP_MAJOR_VERSION", current(explode(".", $PHPVersionInfo))) : NULL;
-	!defined("PHP_MINOR_VERSION") ? define("PHP_MINOR_VERSION", $PHPVersionInfo) : NULL;
-	PHP_MAJOR_VERSION < 5 ? die("Please install PHP 5 or greater in order to use this application.") : NULL;
-
-/*
- * This is the only script which does not use the "$installRoot" instance variable from the "Config" class to include necessary files from unknown directories.
- * The "$installRoot" instaince variable will be made avaliable to all other other PHP scripts once they have included "index.php".
-*/
-	strstr(dirname(__FILE__), "\\") ? $configScript = str_replace("system\server", "", dirname(__FILE__)) . "data\system\config.php" : $configScript = str_replace("system/server", "", dirname(__FILE__)) . "data/system/config.php";
-	require_once($configScript);
+	define("PHP_MINOR_VER",  phpversion());
+	define("PHP_MAJOR_VER", current(explode(".", PHP_MINOR_VER)));
+	PHP_MAJOR_VER < 5 ? die("Please install PHP 5 or greater in order to use this application.") : NULL;
+	
+//Check to see if the system has been setup. If so, the configuration script will exist, and it can be imported, otherwise redirect to the installer.
+	if (IS_WINDOWS) {
+		$configScript = str_replace("system\server", "", dirname(__FILE__)) . "data\system\config.php";
+	} else {
+		$configScript = str_replace("system/server", "", dirname(__FILE__)) . "data/system/config.php";
+	}
+	
+	if (file_exists($configScript)) {
+		require_once($configScript);
+		unset($configScript);
+	} else {
+		if (IS_WINDOWS) {
+			$applicationRouter = explode("system\server", dirname(__FILE__));
+		} else {
+			$applicationRouter = explode("system/server", dirname(__FILE__));
+		}
+		
+		header("Location: http://" . $_SERVER['HTTP_HOST'] . "/" . str_replace($_SERVER['DOCUMENT_ROOT'], "", str_replace("\\", "/", $applicationRouter['0'])) . "setup/index.php");	
+		exit;
+	}
 	
 //Instantiate the "Config" class
 	$config = new Config();
 	
 //Detirmine the root address for the entire site, and include the "http://" if SSL is not active and "https://" if SSL is active
-	$_SERVER['HTTPS'] == "on" ? define("PROTOCOL", "https://") : define("PROTOCOL", "http://");
-	defined("ROOT") ? NULL : define("ROOT", PROTOCOL . $config->installDomain);
-	defined("STRIPPED_ROOT") ? NULL : define("STRIPPED_ROOT", $config->installDomain);
+	isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on" ? define("PROTOCOL", "https://") : define("PROTOCOL", "http://");
+	define("ROOT", PROTOCOL . $config->installDomain);
+	define("STRIPPED_ROOT", $config->installDomain);
 	
 //Include the rest of the system's core. The order of the files in the "$include" array are important! Do not rearrange the order!
-	$include = array("core/logger.php", "core/message.php", "core/database.php");
+	$include = array("core/logger.php", "core/message.php");
 	
 	foreach($include as $script) {
 		require_once($config->installRoot . "system/server/" . $script);
@@ -73,7 +90,53 @@
 //Set server configurations
 	set_time_limit(3600);
 	ini_set("expose_php", "Off");
+	error_reporting(0);
 	
-	/*---------------------------------------------------- Developer use ONLY!!!! Disable during production!!!! ----------------------------------------------------*/
-	error_reporting(-1);
+//Allow additional classes to be imported via ECMAScript standards (e.g. import(package.class))
+	function import($path, $module = "system") {
+		global $config, $message;
+		
+		$importAll = explode(".", $path);
+		
+	//Import an entire package
+		if ($importAll[count($importAll) - 1] == "*") {
+			$modifiedPath = $config->installRoot . $module . "/server/";
+			
+		//- 2 from the iterator, so that the "*" isn't included
+			for ($i = 0; $i <= count($importAll) - 2; $i ++) {
+				$modifiedPath .= $importAll[$i] . "/";
+			}
+			
+		//Check to see if the package exists
+			if (!is_dir($modifiedPath)) {
+				$message->error("<strong>Fatal error:</strong> The system could not import the following classes, because either the package name is incorrect or it does not exist:\n<br /><br />\n" . $path . "\n<br /><br />\nRemember that, by default, import() will only import classes and packages from the system core. To include a package from another module, set the \$module option equal to the name of the module where the given package resides.");
+				
+				exit;
+			}
+			
+			$packageHandler = opendir($modifiedPath);
+			
+		//Get all of the classes from the package
+			while (false !== ($packageContents = readdir($packageHandler))) {
+			//Exclude ".", "..", and any dot files or dot directories
+				if (strpos($packageContents, ".") !== 0) {
+					require_once($modifiedPath . $packageContents);
+				}
+			}
+			
+			closedir($packageHandler);
+	//Import a single class
+		} else {
+			$modifiedPath = $config->installRoot . $module . "/server/" . str_replace(".", "/", $path) . ".php";
+			
+		//Check to see if the package and class exist
+			if (file_exists($modifiedPath)) {
+				require_once($modifiedPath);
+			} else {
+				$message->error("<strong>Fatal error:</strong> The system could not import the following class, because either the package or class name is incorrect or it does not exist:\n<br /><br />\n" . $path . "\n<br /><br />\nRemember that, by default, import() will only import classes and packages from the system core. To include packages and classes from other modules, set the \$module option equal to the name of the module where the given files reside.");
+				
+				exit;
+			}
+		}
+	}
 ?>
